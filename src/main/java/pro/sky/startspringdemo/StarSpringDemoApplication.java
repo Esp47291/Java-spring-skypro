14b2de8035087b97c329fbeb0bb2e1488e86c04b
homecomp <gipno262@gmail.com>
2025-06-07 10:55:47 +0300

    done

---
+++ src/main/java/pro/sky/startspringdemo/StarSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package main.java.pro.sky.startspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/startspringdemo/StarSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.pro.sky.startspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
